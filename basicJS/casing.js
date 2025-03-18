36a59ce8d4c53f0954688cd7a678c2a556593b3b
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-18 10:56:17 +0100

    Added examples of making cararters upper and lower cases

---
+++ basicJS/casing.js
@@ -0,0 +1,7 @@
+var myName = "josselin beaumont"
+var myAge = "i don't want to tell"
+var myDog = "I DONT HAVE A DOG"
+
+console.log(myName.slice(0,1).toUpperCase() + myName.slice(1));
+console.log(myAge.toUpperCase());
+console.log(myDog.toLowerCase());
