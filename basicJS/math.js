775b1a68a51b12b6b206c858c22af5d2692bc6f9
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-20 21:02:33 +0100

    made some changer to math file

--- basicJS/math.js
+++ basicJS/math.js
@@ -3,8 +3,6 @@
 const a = 10;
 const b = 5;
 
-console.log(`Addition: ${a} + ${b} = ${a + b}`);
-console.log(`Subtraction: ${a} - ${b} = ${a - b}`);
 console.log(`Multiplication: ${a} * ${b} = ${a * b}`);
 console.log(`Division: ${a} / ${b} = ${a / b}`);
 console.log(`Modulus: ${a} % ${b} = ${a % b}`);
